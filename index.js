0ab924099806fad0c45f526baba651602d4f7368
David <davidvo1992@gmail.com>
2023-02-26 19:48:54 -0600

    commit tree fb68681c96ef539a40979d4ba727415c688e66c5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.getElementById("main");
+const main = document.getElementById("main"); //we would need to store the element in a variable so we can reference later in the code... idiot...
+main.remove();
+
+document.createElement("h1");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "David Vo is the champion";
